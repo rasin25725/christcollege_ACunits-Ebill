b199790beeb7b73de6107df76d419307b5a8356f
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2026-09-18 11:14:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ import joblib
 import pandas as pd
 
 # Load model (saved as a dict: {"poly": poly, "model": model})
-model_data = joblib.load("electric_bill_ac_fan_polynomial_model.pkl")
+model_data = joblib.load("electric_bill_ac_fan_others_polynomial_model.pkl")
 poly = model_data["poly"]
 model = model_data["model"]
 
 st.title("Electric Bill Predictor")
 
-st.write("Predict Electric Bill using AC and Fan electricity consumption.")
+st.write("Predict Electric Bill using AC Fan and other electricity consumption.")
 
 ac_units = st.number_input("Enter AC Units", min_value=0.0, value=10.0)
 fan_units = st.number_input("Enter Fan Units", min_value=0.0, value=20.0)
+other_units = st.number_input("Enter other Units", min_value=0.0, value=20.0)
 
 if st.button("Predict Bill"):
     new_data = pd.DataFrame({
         "AC_Units": [ac_units],
-        "Fan_Units": [fan_units]
+        "Fan_Units": [fan_units],
+        "Other_Units" : [other_units]
     })
 
     new_data_poly = poly.transform(new_data)
