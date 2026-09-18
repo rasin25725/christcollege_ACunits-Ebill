2517e711067a87ce4b6866ed7a4add62f71de8cd
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2026-09-18 10:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import joblib
-import pandas as pd
 
 model_data = joblib.load("electric_bill_ac_fan_polynomial_model.pkl")
 
@@ -12,14 +11,12 @@ st.title("Electric Bill Predictor")
 st.write("Predict Electric Bill using AC and Fan electricity consumption.")
 
 ac_units = st.number_input("Enter AC Units", min_value=0.0, value=10.0)
+
 fan_units = st.number_input("Enter Fan Units", min_value=0.0, value=20.0)
 
 if st.button("Predict Bill"):
 
-    new_data = pd.DataFrame({
-        "AC_Units": [ac_units],
-        "Fan_Units": [fan_units]
-    })
+    new_data = [[ac_units, fan_units]]
 
     new_data_poly = poly.transform(new_data)
 
